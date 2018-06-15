53f8232abbb485c7c97465440fc360eda705fde8
Nick Charlton <nick@nickcharlton.net>
2018-06-15 16:18:12 +0100

    Return a 404 when docs pages are not found.
    Safari, at least, always tries to load a `apple-touch-icon.png` which
    `DocumentationController` tries to find and render as markdown. This
    leads to the prototype app having a lot of odd errors.

--- spec/features/documentation_spec.rb
+++ spec/features/documentation_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe "documentation navigation" do
+  it "shows a 404 for missing pages" do
+    visit "not_a_page"
+
+    expect(response).to be(not_found)
+  end
+
   it "links to each documentation page" do
     visit root_path
     links = internal_documentation_links
